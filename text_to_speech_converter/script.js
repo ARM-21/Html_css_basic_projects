7d482f3858528a2b53227ba95dd0fee9512c1141
ARM-21 <manojneupane787@gmail.com>
2024-07-13 00:34:32 +0545

    google clone_done

--- text_to_speech_converter/script.js
+++ text_to_speech_converter/script.js
@@ -2,7 +2,20 @@ let speakButton = document.querySelector('.input_section button');
 let input_section = document.querySelector('.input_section input');
 
 speakButton.addEventListener('click', () => {
+    if(input_section.value === ''){
+        alert('Please enter some text');
+        return;
+
+    }
     let text = input_section.value;
     let utterance = new SpeechSynthesisUtterance(text);
     speechSynthesis.speak(utterance);
 });
+
+console.dir(input_section)
+
+input_section.addEventListener('focus', (e) => {
+input_section.placeholder = '';
+console.log('focus')
+
+})
